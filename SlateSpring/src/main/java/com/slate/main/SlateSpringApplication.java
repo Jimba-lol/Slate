03ac23b9e4fb456f86f02872e72f84a61553a64b
knoxiiw <64433273+knoxiiw@users.noreply.github.com>
2020-09-08 17:01:45 -0700

    Back-End Spring Boot
    H2 Database, Spring Web, Spring Data JPA, Spring Security

---
+++ SlateSpring/src/main/java/com/slate/main/SlateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.slate.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlateSpringApplication.class, args);
+	}
+
+}
